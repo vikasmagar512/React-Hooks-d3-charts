84532186fa4e78fefb6f471ef15eabefd87624ea
vikasmagar512@gmail.com <--add>
2020-06-30 14:57:59 +0530

    reusable ag-grid component developed for any json

--- src/Tables/index.js
+++ src/Tables/index.js
@@ -7,43 +7,19 @@ import 'ag-grid-enterprise';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import './Table.css'
+import data from './data.json'
 export default class Table extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      columnDefs: [
-        {
-          field: 'country',
-          rowGroup: true,
-          enableRowGroup: true,
-        },
-        {
-          field: 'year',
-          rowGroup: true,
-          enableRowGroup: true,
-          enablePivot: true,
-        },
-        { field: 'date' },
-        { field: 'sport' },
-        {
-          field: 'gold',
-          aggFunc: 'sum',
-        },
-        {
-          field: 'silver',
-          aggFunc: 'sum',
-        },
-        {
-          field: 'bronze',
-          aggFunc: 'sum',
-        },
-      ],
+      columnDefs: [],
       defaultColDef: {
         flex: 1,
         minWidth: 150,
         sortable: true,
         resizable: true,
+        filter: true,
       },
       autoGroupColumnDef: { minWidth: 250 },
       sideBar: 'columns',
@@ -54,40 +30,65 @@ export default class Table extends Component {
   onGridReady = params => {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
-
-    const httpRequest = new XMLHttpRequest();
     const updateData = data => {
       this.setState({ rowData: data });
     };
+    let columnDefs = Object.keys(data[0]).map((col, i) => {
+      console.log(data[0][col])
 
-    httpRequest.open(
-      'GET',
-      'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinners.json'
-    );
-    httpRequest.send();
-    httpRequest.onreadystatechange = () => {
-      if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-        updateData(JSON.parse(httpRequest.responseText));
+      debugger
+      return {
+        field: col,
+        enableRowGroup: true,
+        enablePivot: true,
+        ...((typeof (data[0][col]) === 'number') ? {
+          filter: 'agNumberColumnFilter',
+          aggFunc: 'sum'
+        } : {
+            ...(col.toLowerCase() !== 'date' ? {
+              filter: 'agTextColumnFilter',
+            } : {
+                filter: 'agDateColumnFilter',
+                filterParams: {
+                  comparator: function (filterLocalDateAtMidnight, cellValue) {
+                    var dateAsString = cellValue;
+                    if (dateAsString == null) return -1;
+                    var dateParts = dateAsString.split('/');
+                    var cellDate = new Date(
+                      Number(dateParts[2]),
+                      Number(dateParts[1]) - 1,
+                      Number(dateParts[0])
+                    );
+                    if (filterLocalDateAtMidnight.getTime() == cellDate.getTime()) {
+                      return 0;
+                    }
+                    if (cellDate < filterLocalDateAtMidnight) {
+                      return -1;
+                    }
+                    if (cellDate > filterLocalDateAtMidnight) {
+                      return 1;
+                    }
+                  },
+                  browserDatePicker: true,
+                },
+              }),
+          })
       }
-    };
+    })
+    this.setState({ columnDefs })
+    updateData(data);
   };
 
   onBtNormal = () => {
     this.gridColumnApi.setPivotMode(false);
-    this.gridColumnApi.setPivotColumns([]);
-    this.gridColumnApi.setRowGroupColumns(['country', 'year']);
   };
 
   onBtPivotMode = () => {
     this.gridColumnApi.setPivotMode(true);
-    this.gridColumnApi.setPivotColumns([]);
-    this.gridColumnApi.setRowGroupColumns(['country', 'year']);
   };
 
   onBtFullPivot = () => {
     this.gridColumnApi.setPivotMode(true);
-    this.gridColumnApi.setPivotColumns(['year']);
-    this.gridColumnApi.setRowGroupColumns(['country']);
   };
 
   render() {
